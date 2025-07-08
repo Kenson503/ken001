d3757070e691c46bca7f953f8653493f4f84bfd8
Kenson503 <kensonmureithi503@gmail.com>
2025-07-08 12:10:06 +0300

    completed my portfolio

---
+++ main.js
@@ -0,0 +1,13 @@
+// Make sure Typed.js is loaded before this script runs.
+// If using a module system, import it like this:
+// import Typed from 'typed.js';
+
+// If using a script tag, ensure this script runs after including Typed.js
+
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Frontend Developer", "Graphic Designer", "Backend Developer", "JavaScript Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
